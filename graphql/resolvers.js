49fda49b7c3920be228eabe18c275ec738103aa2
github username <chang.lim92@gmail.com>
2019-06-19 00:28:57 -0400

    GraphQL Api done

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,15 +1,27 @@
+//
+// import { getMovies, getById, addMovie, deleteMovie } from "./db";
+//
+// const resolvers = {
+//   Query: {
+//     movies: () => getMovies(),
+//     movie: (_, { id }) => getById(id)
+//   },
+//   Mutation: {
+//     addMovie: (_, { name, score }) => addMovie(name, score),
+//     deleteMovie: (_, {id}) => deleteMovie(id)
+//   }
+// }
+//
+// export default resolvers;
 
-import { getMovies, getById, addMovie, deleteMovie } from "./db";
+import { getMovies, getMovie, getSuggestions } from "./db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getById(id)
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, {id}) => deleteMovie(id)
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id)
   }
-}
+};
 
 export default resolvers;
